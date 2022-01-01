9f61c77014b2939a9695c592f97525432af98c17
OrkunAvci <orkunavci28@gmail.com>
2022-01-02 02:33:44 +0300

    Create program.js

---
+++ src/program.js
@@ -0,0 +1,20 @@
+let program;
+let refresh_program = function(gl, vertexShader, fragmentShader)
+{
+	program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		console.error("ERROR linking program!", gl.getProgramInfoLog(program));
+		return;
+	}
+	gl.validateProgram(program);
+	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+		console.error(
+			"ERROR validating program!",
+			gl.getProgramInfoLog(program)
+		);
+		return;
+	}
+}
