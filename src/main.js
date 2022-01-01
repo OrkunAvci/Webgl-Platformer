0a6b397962b83e12db1f5eb54675557a9a9442e0
OrkunAvci <orkunavci28@gmail.com>
2022-01-02 02:34:32 +0300

    Make modular & FIX

--- src/main.js
+++ src/main.js
@@ -1,59 +1,7 @@
-let vertexShaderText = `
-	precision mediump float;
-	
-	attribute vec3 vertPosition;
-	attribute vec2 vertTexCoord;
-	varying vec2 fragTexCoord;
-	uniform mat4 mWorld;
-	uniform mat4 mView;
-	uniform mat4 mProj;
-	uniform vec3 posOffset;
-	varying vec3 positionWithOffset;
-	
-	void main()
-	{
-		fragTexCoord = vertTexCoord;
-		positionWithOffset = vertPosition + posOffset;
-		gl_Position = mProj * mView * mWorld * vec4(positionWithOffset, 1.0);;
-		
-	}
-`
+let posOffsetVec = Float32Array.from([0.0, 0.0, -20.0]);
 
-let fragmentShaderText = `
-	precision mediump float;
-	
-	varying vec2 fragTexCoord;
-	uniform sampler2D sampler;
-	
-	void main()
-	{
-		gl_FragColor = texture2D(sampler, fragTexCoord);
-	}
-`
-
-let posOffsetVec = new Float32Array(3);
-function keyPress(event) {
-	if (event.key == "w") {
-		console.log("W");
-		posOffsetVec[2] += 1.0;
-	} else if (event.key == "s") {
-		console.log("S");
-		posOffsetVec[2] -= 1.0;
-	} else if (event.key == "d") {
-		console.log("D");
-		posOffsetVec[0] -= 1.0;
-	} else if (event.key == "a") {
-		console.log("A");
-		posOffsetVec[0] += 1.0;
-	} else if (event.key == "e") {
-		console.log("E");
-		posOffsetVec[1] += 1.0;
-	} else if (event.key == "f") {
-		console.log("F");
-		posOffsetVec[1] -= 1.0;
-	}
-}
-
+let canvas;
+let gl;
 
 var Init = function () {
 	//	Mandatory
@@ -67,6 +15,12 @@ var Init = function () {
 		alert("Your browser does not support WebGL");
 	}
 
+	//	Setup shaders
+	setup_shaders(gl);
+
+	//	Setup program
+	refresh_program(gl, vertexShader, fragmentShader);
+
 	//	Clean ups and configuration
 	gl.clearColor(0.92, 0.92, 0.92, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -75,222 +29,39 @@ var Init = function () {
 	gl.frontFace(gl.CCW);
 	gl.cullFace(gl.BACK);
 
-	//	Create shaders
-	let vertexShader = gl.createShader(gl.VERTEX_SHADER);
-	let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-	gl.shaderSource(vertexShader, vertexShaderText);
-	gl.shaderSource(fragmentShader, fragmentShaderText);
-	gl.compileShader(vertexShader);
-	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-		console.error(
-			"ERROR compiling vertex shader!",
-			gl.getShaderInfoLog(vertexShader)
-		);
-		return;
-	}
-	gl.compileShader(fragmentShader);
-	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-		console.error(
-			"ERROR compiling fragment shader!",
-			gl.getShaderInfoLog(fragmentShader)
-		);
-		return;
-	}
-
-	//	Setup program
-	let program = gl.createProgram();
-	gl.attachShader(program, vertexShader);
-	gl.attachShader(program, fragmentShader);
-	gl.linkProgram(program);
-	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		console.error("ERROR linking program!", gl.getProgramInfoLog(program));
-		return;
-	}
-	gl.validateProgram(program);
-	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-		console.error(
-			"ERROR validating program!",
-			gl.getProgramInfoLog(program)
-		);
-		return;
-	}
-
-	//	Create box
-	let boxVertices = [
-		// X, Y, Z           U, V
-		// Top
-		-1.0, 1.0, -1.0, 	0, 0,
-		-1.0, 1.0, 1.0, 	0, 1,
-		1.0, 1.0, 1.0, 		1, 1,
-		1.0, 1.0, -1.0, 	1, 0,
-
-		// Left
-		-1.0, 1.0, 1.0, 	0, 0,
-		-1.0, -1.0, 1.0, 	1, 0,
-		-1.0, -1.0, -1.0, 	1, 1,
-		-1.0, 1.0, -1.0, 	0, 1,
-
-		// Right
-		1.0, 1.0, 1.0, 		1, 1, 
-		1.0, -1.0, 1.0, 	0, 1, 
-		1.0, -1.0, -1.0, 	0, 0,
-		1.0, 1.0, -1.0, 	1, 0,
-
-		// Front
-		1.0, 1.0, 1.0, 		1, 1,
-		1.0, -1.0, 1.0, 	1, 0,
-		-1.0, -1.0, 1.0, 	0, 0,
-		-1.0, 1.0, 1.0, 	0, 1,
-
-		// Back
-		1.0, 1.0, -1.0, 	0, 0,
-		1.0, -1.0, -1.0, 	0, 1,
-		-1.0, -1.0, -1.0, 	1, 1,
-		-1.0, 1.0, -1.0, 	1, 0,
-
-		// Bottom
-		-1.0, -1.0, -1.0, 	1, 1,
-		-1.0, -1.0, 1.0, 	1, 0,
-		1.0, -1.0, 1.0, 	0, 0,
-		1.0, -1.0, -1.0, 	0, 1,
-	];
-	let boxIndices = [
-		// Top
-		0, 1, 2,
-		0, 2, 3,
-
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23,
-	];
-	let boxVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-	gl.bufferData(
-		gl.ARRAY_BUFFER,
-		new Float32Array(boxVertices),
-		gl.STATIC_DRAW
-	);
-	let boxIndexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-	gl.bufferData(
-		gl.ELEMENT_ARRAY_BUFFER,
-		new Uint16Array(boxIndices),
-		gl.STATIC_DRAW
-	);
-	let positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
-	let texCoordAttribLocation = gl.getAttribLocation(program, "vertTexCoord");
-	gl.vertexAttribPointer(
-		positionAttribLocation, // Attribute location
-		3, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		0 // Offset from the beginning of a single vertex to this attribute
-	);
-	gl.vertexAttribPointer(
-		texCoordAttribLocation, // Attribute location
-		2, // Number of elements per attribute
-		gl.FLOAT, // Type of elements
-		gl.FALSE,
-		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
-	);
-	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(texCoordAttribLocation);
-
-	// Create texture
-	let boxTexture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-	gl.texImage2D(
-		gl.TEXTURE_2D,
-		0,
-		gl.RGBA,
-		gl.RGBA,
-		gl.UNSIGNED_BYTE,
-		document.getElementById("create-texture")
-	);
-	gl.bindTexture(gl.TEXTURE_2D, null);
-
-	// Tell OpenGL state machine which program should be active.
 	gl.useProgram(program);
 
-	let matWorldUniformLocation = gl.getUniformLocation(program, "mWorld");
-	let matViewUniformLocation = gl.getUniformLocation(program, "mView");
-	let matProjUniformLocation = gl.getUniformLocation(program, "mProj");
-	let objPositionOffsetLocation = gl.getUniformLocation(program, "posOffset");
+	objects.forEach(obj => draw(gl, obj));
 
-	//	Setup camera
-	let worldMatrix = new Float32Array(16);
-	let viewMatrix = new Float32Array(16);
-	let projMatrix = new Float32Array(16);
-	mat4.identity(worldMatrix);
-	mat4.lookAt(viewMatrix, [-25, 25, -25], [0, 0, 0], [0, 1, 0]);
-	mat4.perspective(
-		projMatrix,
-		glMatrix.toRadian(45),
-		canvas.clientWidth / canvas.clientHeight,
-		0.1,
-		1000.0
-	);
+	texture_ids.forEach(id => {
+		textures.push(make_texture(gl, id));
+	})
+
+	let createLoc = gl.getUniformLocation(program, "create");
+	let wallLoc = gl.getUniformLocation(program, "wall");
 	
-	//	Init position of box
-	posOffsetVec= Float32Array.from([0.0, 0.0, -10.0]);
-
-	//	Assign computed values to shader variables
-	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-	gl.uniform3fv(objPositionOffsetLocation, posOffsetVec);
+	
+	gl.uniform1i(createLoc, 0);
+	gl.uniform1i(wallLoc, 1);
 
 	//	Define actions
-	document.addEventListener("keyup", keyPress, false);
+	document.addEventListener("keyup", move, false);
 
 	// Main render loop
-	let xRotationMatrix = new Float32Array(16);
-	let yRotationMatrix = new Float32Array(16);
-	let identityMatrix = new Float32Array(16);
-	mat4.identity(identityMatrix);
-	let angle = 0;
 	let render = function () {
-		//	Change angle (Disabled)
-		angle = (performance.now() / 1000 / 6) * 2 * Math.PI;
-		mat4.rotate(yRotationMatrix, identityMatrix, 0, [0, 1, 0]);
-		mat4.rotate(xRotationMatrix, identityMatrix, 0, [1, 0, 0]);
-		mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
-		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-		//	Move position
-		gl.uniform3fv(objPositionOffsetLocation, posOffsetVec);
 
 		//	Reset background
 		gl.clearColor(0.92, 0.92, 0.92, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-		//	Use texture
-		gl.bindTexture(gl.TEXTURE_2D, boxTexture);
-		gl.activeTexture(gl.TEXTURE0);
-
-		//	Draw
-		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+		for (i=0; i<objects.length; i++)
+		{
+			obj = objects[i];
+			gl.activeTexture(gl.TEXTURE0 + i);
+			gl.bindTexture(gl.TEXTURE_2D, textures[i]);
+			draw(gl, obj);
+			gl.drawElements(gl.TRIANGLES, obj.indices.length, gl.UNSIGNED_SHORT, 0);
+		}
 
 		//	Recursive
 		requestAnimationFrame(render);
