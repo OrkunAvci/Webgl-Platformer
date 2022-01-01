94ef79a0253ab79c83a07d5cb90ca306e57ad754
OrkunAvci <orkunavci28@gmail.com>
2022-01-02 02:33:34 +0300

    Create textures.js

---
+++ src/textures.js
@@ -0,0 +1,35 @@
+let texture_ids = [
+	"create-texture",
+	"wall-texture"
+];
+
+let textures = [];
+
+function make_texture(gl, id)
+{
+	let texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.texParameteri(
+		gl.TEXTURE_2D,
+		gl.TEXTURE_WRAP_S,
+		gl.CLAMP_TO_EDGE
+	);
+	gl.texParameteri(
+		gl.TEXTURE_2D,
+		gl.TEXTURE_WRAP_T,
+		gl.CLAMP_TO_EDGE
+	);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		document.getElementById(id)
+	);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+
+	return texture;
+}
