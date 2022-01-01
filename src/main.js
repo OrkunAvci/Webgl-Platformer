136088eed4f0f348b9c8fa9caa0d8fb4302ec64c
OrkunAvci <orkunavci28@gmail.com>
2022-01-01 21:18:05 +0300

    Fix EVERYTHING and comments

--- src/main.js
+++ src/main.js
@@ -1,27 +1,33 @@
-let cube = null;
-
 let vertexShaderText = `
 	precision mediump float;
 	
 	attribute vec3 vertPosition;
+	attribute vec2 vertTexCoord;
 	varying vec2 fragTexCoord;
 	uniform mat4 mWorld;
 	uniform mat4 mView;
 	uniform mat4 mProj;
 	uniform vec3 posOffset;
+	varying vec3 positionWithOffset;
 	
 	void main()
 	{
-	  gl_Position = mProj * mView * mWorld * vec4(vertPosition + posOffset, 1.0);
+		fragTexCoord = vertTexCoord;
+		positionWithOffset = vertPosition + posOffset;
+		gl_Position = mProj * mView * mWorld * vec4(positionWithOffset, 1.0);;
+		
 	}
 `
 
 let fragmentShaderText = `
 	precision mediump float;
 	
+	varying vec2 fragTexCoord;
+	uniform sampler2D sampler;
+	
 	void main()
 	{
-		
+		gl_FragColor = texture2D(sampler, fragTexCoord);
 	}
 `
 
@@ -50,34 +56,30 @@ function keyPress(event) {
 
 
 var Init = function () {
+	//	Mandatory
 	let canvas = document.getElementById("game-surface");
 	let gl = canvas.getContext("webgl");
-
 	if (!gl) {
 		console.log("WebGL not supported, falling back on experimental-webgl");
 		gl = canvas.getContext("experimental-webgl");
 	}
-
 	if (!gl) {
 		alert("Your browser does not support WebGL");
 	}
 
-	gl.clearColor(0.12, 0.12, 0.12, 1.0);
+	//	Clean ups and configuration
+	gl.clearColor(0.92, 0.92, 0.92, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 	gl.enable(gl.DEPTH_TEST);
 	gl.enable(gl.CULL_FACE);
 	gl.frontFace(gl.CCW);
 	gl.cullFace(gl.BACK);
 
-	//
-	// Create shaders
-	//
+	//	Create shaders
 	let vertexShader = gl.createShader(gl.VERTEX_SHADER);
 	let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-
 	gl.shaderSource(vertexShader, vertexShaderText);
 	gl.shaderSource(fragmentShader, fragmentShaderText);
-
 	gl.compileShader(vertexShader);
 	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
 		console.error(
@@ -86,7 +88,6 @@ var Init = function () {
 		);
 		return;
 	}
-
 	gl.compileShader(fragmentShader);
 	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
 		console.error(
@@ -96,6 +97,7 @@ var Init = function () {
 		return;
 	}
 
+	//	Setup program
 	let program = gl.createProgram();
 	gl.attachShader(program, vertexShader);
 	gl.attachShader(program, fragmentShader);
@@ -113,48 +115,45 @@ var Init = function () {
 		return;
 	}
 
-	//
-	// Create buffer
-	//
+	//	Create box
 	let boxVertices = [
 		// X, Y, Z           U, V
 		// Top
-		-1.0, 1.0, -1.0,
-		-1.0, 1.0, 1.0,
-		1.0, 1.0, 1.0,
-		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0, 	0, 0,
+		-1.0, 1.0, 1.0, 	0, 1,
+		1.0, 1.0, 1.0, 		1, 1,
+		1.0, 1.0, -1.0, 	1, 0,
 
 		// Left
-		-1.0, 1.0, 1.0,
-		-1.0, -1.0, 1.0,
-		-1.0, -1.0, -1.0,
-		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0, 	0, 0,
+		-1.0, -1.0, 1.0, 	1, 0,
+		-1.0, -1.0, -1.0, 	1, 1,
+		-1.0, 1.0, -1.0, 	0, 1,
 
 		// Right
-		1.0, 1.0, 1.0,
-		1.0, -1.0, 1.0, 
-		1.0, -1.0, -1.0,
-		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0, 		1, 1, 
+		1.0, -1.0, 1.0, 	0, 1, 
+		1.0, -1.0, -1.0, 	0, 0,
+		1.0, 1.0, -1.0, 	1, 0,
 
 		// Front
-		1.0, 1.0, 1.0,
-		1.0, -1.0, 1.0,
-		-1.0, -1.0, 1.0,
-		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0, 		1, 1,
+		1.0, -1.0, 1.0, 	1, 0,
+		-1.0, -1.0, 1.0, 	0, 0,
+		-1.0, 1.0, 1.0, 	0, 1,
 
 		// Back
-		1.0, 1.0, -1.0,
-		1.0, -1.0, -1.0,
-		-1.0, -1.0, -1.0,
-		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0, 	0, 0,
+		1.0, -1.0, -1.0, 	0, 1,
+		-1.0, -1.0, -1.0, 	1, 1,
+		-1.0, 1.0, -1.0, 	1, 0,
 
 		// Bottom
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0, 1.0,
-		1.0, -1.0, 1.0,
-		1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0, 	1, 1,
+		-1.0, -1.0, 1.0, 	1, 0,
+		1.0, -1.0, 1.0, 	0, 0,
+		1.0, -1.0, -1.0, 	0, 1,
 	];
-
 	let boxIndices = [
 		// Top
 		0, 1, 2,
@@ -180,7 +179,6 @@ var Init = function () {
 		21, 20, 22,
 		22, 20, 23,
 	];
-
 	let boxVertexBufferObject = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
 	gl.bufferData(
@@ -188,7 +186,6 @@ var Init = function () {
 		new Float32Array(boxVertices),
 		gl.STATIC_DRAW
 	);
-
 	let boxIndexBufferObject = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
 	gl.bufferData(
@@ -196,8 +193,8 @@ var Init = function () {
 		new Uint16Array(boxIndices),
 		gl.STATIC_DRAW
 	);
-
 	let positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
+	let texCoordAttribLocation = gl.getAttribLocation(program, "vertTexCoord");
 	gl.vertexAttribPointer(
 		positionAttribLocation, // Attribute location
 		3, // Number of elements per attribute
@@ -206,8 +203,33 @@ var Init = function () {
 		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
 		0 // Offset from the beginning of a single vertex to this attribute
 	);
-
+	gl.vertexAttribPointer(
+		texCoordAttribLocation, // Attribute location
+		2, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+	);
 	gl.enableVertexAttribArray(positionAttribLocation);
+	gl.enableVertexAttribArray(texCoordAttribLocation);
+
+	// Create texture
+	let boxTexture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	gl.texImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		document.getElementById("create-texture")
+	);
+	gl.bindTexture(gl.TEXTURE_2D, null);
 
 	// Tell OpenGL state machine which program should be active.
 	gl.useProgram(program);
@@ -217,13 +239,12 @@ var Init = function () {
 	let matProjUniformLocation = gl.getUniformLocation(program, "mProj");
 	let objPositionOffsetLocation = gl.getUniformLocation(program, "posOffset");
 
-	document.addEventListener("keyup", keyPress, false);
-
+	//	Setup camera
 	let worldMatrix = new Float32Array(16);
 	let viewMatrix = new Float32Array(16);
 	let projMatrix = new Float32Array(16);
 	mat4.identity(worldMatrix);
-	mat4.lookAt(viewMatrix, [-10, 15, -15], [0, 0, 0], [0, 1, 0]);
+	mat4.lookAt(viewMatrix, [-25, 25, -25], [0, 0, 0], [0, 1, 0]);
 	mat4.perspective(
 		projMatrix,
 		glMatrix.toRadian(45),
@@ -231,38 +252,47 @@ var Init = function () {
 		0.1,
 		1000.0
 	);
+	
+	//	Init position of box
+	posOffsetVec= Float32Array.from([0.0, 0.0, -10.0]);
 
+	//	Assign computed values to shader variables
 	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
 	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
 	gl.uniform3fv(objPositionOffsetLocation, posOffsetVec);
 
-	//
+	//	Define actions
+	document.addEventListener("keyup", keyPress, false);
+
 	// Main render loop
-	//
 	let xRotationMatrix = new Float32Array(16);
 	let yRotationMatrix = new Float32Array(16);
 	let identityMatrix = new Float32Array(16);
-	let xMovementMatrix = new Float32Array(16);
-	let yMovementMatrix = new Float32Array(16);
-	let moveIdentityMatrix = new Float32Array(16);
 	mat4.identity(identityMatrix);
-	mat4.identity(moveIdentityMatrix);
 	let angle = 0;
 	let render = function () {
+		//	Change angle (Disabled)
 		angle = (performance.now() / 1000 / 6) * 2 * Math.PI;
 		mat4.rotate(yRotationMatrix, identityMatrix, 0, [0, 1, 0]);
 		mat4.rotate(xRotationMatrix, identityMatrix, 0, [1, 0, 0]);
 		mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
 		gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-		//gl.uniform3fv(objPositionOffsetLocation, gl.FALSE, posOffsetVec);
+		//	Move position
 		gl.uniform3fv(objPositionOffsetLocation, posOffsetVec);
 
-		gl.clearColor(0.12, 0.12, 0.12, 1.0);
+		//	Reset background
+		gl.clearColor(0.92, 0.92, 0.92, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
+		//	Use texture
+		gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+		gl.activeTexture(gl.TEXTURE0);
+
+		//	Draw
 		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 
+		//	Recursive
 		requestAnimationFrame(render);
 	};
 	requestAnimationFrame(render);
